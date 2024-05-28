ba6348b2a250e90c30f050faa455aec9272af21d
Sheyalaura <sheyalaura@gmail.com>
2024-05-28 19:14:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function showme(){
+    let nav = document.getElementById('bar');
+
+    
+    if (nav.style.display == 'none'){
+        nav.style.display = 'flex';
+    } else {
+        nav.style.display =  'none';
+    }
+
+    
+}
